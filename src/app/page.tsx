28c29c4a3d0d618323a5cbc2ce46a8026c9ebb7a
Tommy Yan <tylyan@gmail.com>
2024-12-05 22:47:30 -0800

    add storybook and radix themes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,14 @@
-import App from '../App';
+import { Flex, Text, Button, Box, Container } from '@radix-ui/themes';
 
 export default function Home() {
-  return <App />;
+  return (
+    <Box style={{ background: 'var(--gray-a2)', borderRadius: 'var(--radius-3)' }}>
+      <Container size="1">
+        <Flex direction="column" gap="2">
+          <Text>Hello from Radix Themes :)</Text>
+          <Button>Let's go</Button>
+        </Flex>
+      </Container>
+    </Box>
+  );
 }
